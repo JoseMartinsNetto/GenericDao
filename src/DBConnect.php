f7888aba2d3eb829c683657041a47bf1ea1f23b9
jmsantosnetto <j.msantos.netto@gmail.com>
2018-06-05 23:53:44 -0300

    Added defaul atributes to connection (auto utf8 encoding, auto fetch assoc), and fixed excepition error

--- src/DBConnect.php
+++ src/DBConnect.php
@@ -5,26 +5,28 @@ require 'IDBConnect.php';
 require 'DBConfig.php';
 
 use PDO;
+use PDOException;
 
 class DBConnect implements IDBConnect
 {
     private static $db;
     private const DSN = DBConfig::DB_TYPE .':host=' . DBConfig::DB_HOST . ';dbname=' . DBConfig::DB_NAME;
-
+    
     public static function connect(): void
     {
         try {
-			self::$db = new PDO(self::DSN, DBConfig::DB_USER,  DBConfig::DB_PASS);
+            self::$db = new PDO(self::DSN, DBConfig::DB_USER,  DBConfig::DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         } catch (PDOException $error) {
             exit('Error to connect to DataBase: Error returned:>>' . $error->getMessage());
         }
     }
-
+    
     public static function getConnection()
     {
         self::connect();
         return self::$db;
     }
-
+    
 }
